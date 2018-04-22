6b087f27d24c99d4e5fb3e7d8aef1ebab4415522
Chirag Mahaveer Parmar <chiragparmar12209@gmail.com>
2018-04-22 14:32:23 -0700

    backend code finished

--- back.js
+++ back.js
@@ -9,8 +9,10 @@ var bodyparser = require('body-parser')
 base_url = "https://maps.googleapis.com/maps/api/directions/json?"
 API_KEY = "AIzaSyBV6hBZ1SWDO8lSAUNpiltBMdrYxoikz3Q"
 
+app.use(bodyparser.json({ type: 'application/json' }));
+
 //make a request to directions api
-var make_request = function(origin, destination, mode){
+var make_request = function(origin, destination, mode, res){
 	URL = base_url + "origin=" + origin.replace(" ","+") + "&destination=" + destination.replace(" ", "+") + "&mode=" + mode + "&key=" + API_KEY
 	request(URL, function(error, response, body){
 		if(!error){
@@ -20,7 +22,7 @@ var make_request = function(origin, destination, mode){
 			turn_directions = []
 			distance_to_travel = []
 			duration_to_travel = []
-			console.log(steps)
+			var destination_loc = ""
 			for(var i=0; i < num_steps; i++){
 				if (i==0){
 					if (steps[i]["html_instructions"].indexOf("east") > -1){
@@ -39,12 +41,24 @@ var make_request = function(origin, destination, mode){
 				else{
 					turn_directions[i] = steps[i]["maneuver"]
 				}
+				if(i==(num_steps-1)){
+					preString = "Destination"
+  					preIndex = steps[i]["html_instructions"].indexOf(preString)
+  					preString = steps[i]["html_instructions"].substring(preIndex)
+  					if(preString.indexOf("left") > -1){
+  						destination_loc = "left"
+  					}
+  					else if (preString.indexOf("right") > -1){
+  						destination_loc = "right"
+  					}
+				}
 				distance_to_travel[i] = steps[i]["distance"]["value"]
 				duration_to_travel[i] = steps[i]["duration"]["value"]
 			}
 			console.log(turn_directions)
 			console.log(distance_to_travel)
 			console.log(duration_to_travel)
+			console.log(destination_loc)
 			var turn_directions_string = ""
 			var distance_string = ""
 			var duration_string = ""
@@ -54,19 +68,26 @@ var make_request = function(origin, destination, mode){
 				distance_string += distance_to_travel[j] + ","
 				duration_string += duration_to_travel[j] + ","
 			}
-			var masterString = turn_directions_string + "|" + distance_string + "|" + duration_string
-			console.log(masterString)
+			var masterString = turn_directions_string + "|" + distance_string + "|" + duration_string + "|" + destination_loc
+			res.end(masterString)
 		}
 		else{
-			console.log("failed")
+			res.send(500, "Couldn't complete request")
 		}
 	})
 }
 
 // Our handler function is passed a request and response object
-app.get('/', function(req, res) {
-  console.log(req)
-  res.end();
+app.post('/', function(req, res) {
+  if(req.body.walkity_key == "walkity786"){
+  	setTimeout(function(){
+  		make_request(req.body.origin, req.body.destination, req.body.mode, res)
+  	},10)
+  }
+  else{
+  	res.send(500,"Failed Request")
+  }
 });
 
 app.listen(8000)
+console.log("App listening on port 8000")
