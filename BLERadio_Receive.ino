f639aaf7e47e5e25a3f4f818807d001615efc3a2
anithp <patelanith01@gmail.com>
2018-05-10 19:32:56 -0700

    Intr BLE

--- BLERadio_Receive.ino
+++ BLERadio_Receive.ino
@@ -1,60 +1,106 @@
+#include <AltSoftSerial.h>
 #include<SPI.h>
 #include "RF24.h"
 #include "nRF24L01.h"
+#include "Arduino.h"
+#include <avr/sleep.h>
+#include <avr/power.h>
+#include <avr/wdt.h>
 
-RF24 myRadio(7, 8);
+const int buttonPin = 2;  //Interrupt
+int buttonState = 0;
 
+RF24 myRadio(2, 3);
 byte addresses[][6] = {"1"};
-int flag=0;
+ 
+AltSoftSerial BTserial;  
+boolean NL = true;
+
+int send_num=random(0,6);
 struct package
 {
   char c=' ';
-  boolean connection = false;
-  int check_send=0;
+  boolean connection=false;
+  int check_send=send_num;
   int read_num=0;
 };
 
 typedef struct package Package;
 Package data;
 
+
+boolean Checkconnection(int checks)
+{
+  int check2=checks-3;
+  Serial.print("Check2=");
+  Serial.println(check2);
+  if(check2 ==send_num)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+void enterSleep(void)
+{
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  sleep_enable();
+  sleep_mode();
+}
+
+void exitSleep(void)
+{
+  sleep_disable();
+  power_all_enable();
+}
+
+void pin_ISR() {
+  buttonState = digitalRead(buttonPin);
+  if(buttonState == 1){
+    exitSleep();
+    }  
+}
+
 void Transmit()
 {
-    myRadio.stopListening();
-    myRadio.openWritingPipe(addresses[0]);
-    boolean check=myRadio.write(&data, sizeof(data));
-    if(check)
-    {
-      Serial.println("Ok");
-      Serial.print("Data Transmitted: ");
-    Serial.println(data.read_num);
-    }
-    else
-    {
-      Transmit();
-    }
+  myRadio.stopListening();
+  myRadio.openWritingPipe(addresses[0]);
+  boolean check=myRadio.write(&data, sizeof(data));
+  if(!check && data.connection==false)
+  {
+    Transmit();
+  }
+  else
+  {
+    Serial.println("ok");
     
-    
+  }
 }
+
 void Receive()
 {
-  myRadio.startListening();
+    myRadio.startListening();
     if(myRadio.available())
     {
       myRadio.read(&data, sizeof(data));
+      data.connection=Checkconnection(data.read_num);
     }
-    else if(data.connection == false)
+    else
     {
-      Serial.println("NO DATA RECEIVED");
+      Serial.println("Data Not Received");
       Receive();
     }
-  }
-
+}
 
 void Vibrate()
 {
+
   switch(data.c)
   {
-    case 'L':
+    case 'R':
     
     //for(int x=0; x<=153; x++)
     //{
@@ -77,7 +123,7 @@ void Vibrate()
     data.c=' ';
     break;
 
-    case 'l':
+    case 'r':
 
     digitalWrite(5, HIGH);
     delay(1500);
@@ -90,8 +136,8 @@ void Vibrate()
     //  delay(10);
    // }
     
-    //delay(800);
-    //analogWrite(5, 0);
+    delay(800);
+    analogWrite(5, 0);
     data.c=' ';
     break;
 
@@ -142,6 +188,12 @@ void Vibrate()
     data.c=' ';
     break;
 
+    case 'x':
+    data.c=' ';
+    enterSleep();
+    break;
+    
+
     case 'd':
 
     digitalWrite(5, HIGH);
@@ -167,44 +219,87 @@ void Vibrate()
     default:
     Serial.println("Wrong character entered");
     data.c=' ';
-  }   
- 
+  }
+  
   
 }
 
-void setup() {
-  Serial.begin(9600);
-  delay(100);
+ 
+ 
+void Bluetooth()
+{
+   if (BTserial.available())
+    {
+      delay(10);
+        data.c = BTserial.read();
+    }
+ 
+ 
+    
+    if (Serial.available())
+    {
+      delay(10);
+        data.c = Serial.read();
+ 
+       
+        if (data.c!=10 & data.c!=13 ) 
+        {  
+             BTserial.write(data.c);
+        }
+ 
+        if (NL) { Serial.print("\r\n>");  NL = false; }
+        Serial.write(data.c);
+        if (data.c==10) { NL = true; }
+    }
+ }
+ 
+void setup() 
+{
+    Serial.begin(9600);
+    Serial.print("Sketch:   ");   Serial.println(__FILE__);
+    Serial.print("Uploaded: ");   Serial.println(__DATE__);
+    Serial.println(" ");
+ 
+    BTserial.begin(9600);  
+    Serial.println("BTserial started at 9600");
+    delay(1000);
+
   myRadio.begin();
   myRadio.setChannel(115);
   myRadio.setPALevel(RF24_PA_MAX);
   myRadio.setDataRate(RF24_250KBPS);
-  myRadio.openReadingPipe(1, addresses[0]);
+  myRadio.openReadingPipe(1,addresses[0]);
+  delay(1000);
+
   myRadio.printDetails();
+  pinMode(5, OUTPUT);
+  digitalWrite(4, HIGH);
+  digitalWrite(7, HIGH);
 
-pinMode(5, OUTPUT);  
-  }
-
-void loop() 
+   pinMode(buttonPin, INPUT);
+  // Attach an interrupt to the ISR vector
+  attachInterrupt(0, pin_ISR, CHANGE);
+}
+ 
+void loop()
 {
-  
-  Receive();
+  Bluetooth();
+  Transmit();
   delay(50);
-  Serial.print("Number Received: ");
+  myRadio.setRetries(15,15);
+  Serial.print("Number Transmitted: ");
   Serial.println(data.check_send);
-   if(data.connection == false)
-   {
-    data.read_num=data.check_send+3;
-    Transmit();
-    myRadio.setRetries(15,15);
+  if(data.connection==false)
+  {
+    Receive();
     delay(500);
-   }
-   else
-   {
+  }
+  else
+  {
     Serial.println("Connection Established");
     Serial.print("Character Received: ");
     Serial.println(data.c);
     Vibrate();
-   }
-    
+  }
+  
 }
